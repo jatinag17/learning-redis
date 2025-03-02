7165f8e061c00522eae9fe2f1a3d46b952e80047
jatinag17 <jatinagrawal128@gmail.com>
2025-03-02 22:58:34 +0530

    update string.js to set expiration for Redis message retrieval

--- string.js
+++ string.js
@@ -1,7 +1,8 @@
 const client=require('./client');
 
 async function init(){
-    await client.set('msg:6',"hey from Nodejs");
+    // await client.set('msg:6',"hey from Nodejs");
+   await client.expire("msg:6",10); 
    const result= await client.get('msg:6');
    console.log("Result ->",result);
 }
