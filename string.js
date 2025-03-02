873c2cc86e03a82d0b9017901f72f8edd6306d48
jatinag17 <jatinagrawal128@gmail.com>
2025-03-02 14:18:31 +0530

    update string.js to set and retrieve a message from Redis

--- string.js
+++ string.js
@@ -1,7 +1,8 @@
 const client=require('./client');
 
 async function init(){
-   const result= await client.get('user:1');
+    await client.set('msg:6',"hey from Nodejs");
+   const result= await client.get('msg:6');
    console.log("Result ->",result);
 }
 
